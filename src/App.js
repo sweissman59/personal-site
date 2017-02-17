368e6739597b40f5b42303b9b3c9a23270b5bad7
Sam Weissman <samw@vadio.com>
2017-02-16 20:04:42 -0800

    refactored nav bar into its own component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './static/css/App.css'
 import Gallery from './static/js/Gallery.js'
 import About from './static/js/About.js'
 import Resume from './static/js/Resume.js'
-import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap'
+import NavBar from './static/js/NavBar.js'
+
 import $ from 'jquery'
 
 class App extends Component {
@@ -33,34 +34,11 @@ class App extends Component {
       display = <Resume />;
     }
 
-    let socialButtons = [
-      <Button key="github-button" className="btn-social-icon btn-github" href="https://github.com/sweissman59" aria-label="Github"><span className="fa fa-github"></span></Button>,
-      <Button key="linkedin-button" className="btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/sam-weissman-hohler" aria-label="Linked In"><span className="fa fa-linkedin"></span></Button>,
-      <Button key="twitter-button" className="btn-social-icon btn-twitter" href="https://twitter.com/tacroy47" aria-label="Twitter"><span className="fa fa-twitter"></span></Button>,
-      <Button key="flickr-button" className="btn-social-icon btn-flickr" href="https://www.flickr.com/photos/phixional_ninja/" aria-label="Flickr"><span className="fa fa-flickr"></span></Button>
-    ];
-    if ($(window).width() < 410) {
-      socialButtons = (
-        <DropdownButton id="social-dropdown" className="fa fa-bars" aria-label="Social Media">
-          { socialButtons.map(function(button, index) {
-            let href = button.props.href;
-            button = React.cloneElement(button, {href: null});
-            return (<MenuItem key={index} href={ href }>{ button }</MenuItem>)
-          }) }
-        </DropdownButton>
-      )
-    }
-
     return (
       <div className="App">
         <div className="App-header">
           <h1>Sam Weissman-Hohler</h1>
-          <ButtonGroup id='navButtons'>
-            <Button className='navButton' active={this.state.page === 'gallery'} onClick={() => this.navClick('gallery')}>Gallery</Button>
-            <Button className='navButton' active={this.state.page === 'about'} onClick={() => this.navClick('about')}>About</Button>
-            <Button className='navButton' active={this.state.page === 'resume'} onClick={() => this.navClick('resume')}>Resume</Button>
-            { socialButtons }
-          </ButtonGroup>
+          <NavBar page={this.state.page} navClick={this.navClick.bind(this)}/>
         </div>
         {display}
       </div>
---
+++ src/static/js/NavBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap'
+import $ from 'jquery'
+import '../css/NavBar.css'
+
+class NavBar extends Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		let socialButtons = [
+			<Button key="github-button" className="btn-social-icon btn-github" href="https://github.com/sweissman59" aria-label="Github"><span className="fa fa-github"></span></Button>,
+			<Button key="linkedin-button" className="btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/sam-weissman-hohler" aria-label="Linked In"><span className="fa fa-linkedin"></span></Button>,
+			<Button key="twitter-button" className="btn-social-icon btn-twitter" href="https://twitter.com/tacroy47" aria-label="Twitter"><span className="fa fa-twitter"></span></Button>,
+			<Button key="flickr-button" className="btn-social-icon btn-flickr" href="https://www.flickr.com/photos/phixional_ninja/" aria-label="Flickr"><span className="fa fa-flickr"></span></Button>
+		];
+		if ($(window).width() < 410) {
+			socialButtons = (
+				<DropdownButton id="social-dropdown" className="fa fa-bars" aria-label="Social Media">
+					{ socialButtons.map(function(button, index) {
+						let href = button.props.href;
+						button = React.cloneElement(button, {href: null});
+						return (<MenuItem key={index} href={ href }>{ button }</MenuItem>)
+					}) }
+				</DropdownButton>
+			)
+		}
+
+		return (
+		<ButtonGroup id='navButtons'>
+			<Button className='navButton' active={this.props.page === 'gallery'} onClick={() => this.props.navClick('gallery')}>Gallery</Button>
+			<Button className='navButton' active={this.props.page === 'about'} onClick={() => this.props.navClick('about')}>About</Button>
+			<Button className='navButton' active={this.props.page === 'resume'} onClick={() => this.props.navClick('resume')}>Resume</Button>
+			{ socialButtons }
+		</ButtonGroup>
+		)
+	}
+}
+
+export default NavBar
