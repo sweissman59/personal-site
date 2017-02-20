7e233cbb081412241481a53c29a5e30d60021610
Sam Weissman <samw@vadio.com>
2017-02-20 13:27:03 -0800

    resume embed with object and fallback png

--- src/static/js/Resume.js
+++ src/static/js/Resume.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
+import { Well } from 'react-bootstrap'
+import '../css/Resume.css'
+import PDF from 'react-pdf-js'
 
 class Resume extends Component {
 	render () {
-		return (<div>Be impressive here</div>);
+		return (
+		<Well className="resume-container">
+			<object id="resume-pdf" type="application/pdf" data="/SamWeissman-HohlerResume.pdf">
+				<img id="resume-png" src="/SamWeissman-HohlerResume.png" />
+			</object>
+		</Well>
+		)
 	}
 }
 
